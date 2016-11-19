20f54d7ea28e073efbdb31a53d3d8c32ca6620e2
Michael Strickmann <strickmann.michael@outlook.com>
2016-11-19 21:11:00 +0100

    Initial

---
+++ NeaUtils/Extensions/ErrorHandling/ErrorChecks.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace NeaUtils.Extensions.ErrorHandling
+{
+    internal static class ErrorChecks
+    {
+        public static InvalidValueException ConditionalErrorCheck<T>(this T source, Expression<Func<T, bool>> exp, bool autothrow = false)
+        {
+            if (!exp.Compile()(source))
+            {
+                return null;
+            }
+            
+            var ex = new InvalidValueException("The Value " + source + " violated the predicate Function: " + exp.Body);
+            if (autothrow)
+            {
+                throw ex;
+            }
+            return ex;
+        }
+    }
+
+    public class InvalidValueException : Exception
+    {
+        public InvalidValueException()
+        {
+        }
+
+        public InvalidValueException(string message)
+        : base(message)
+        {
+        }
+
+        public InvalidValueException(string message, Exception inner)
+        : base(message, inner)
+        {
+        }
+    }
+}
