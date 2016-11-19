bfc670bd950a15e2bfd8244ebfb0901e4c97b573
Michael Strickmann <strickmann.michael@outlook.com>
2016-11-19 21:32:52 +0100

    Comments

--- NeaUtils/Extensions/ErrorHandling/ErrorChecks.cs
+++ NeaUtils/Extensions/ErrorHandling/ErrorChecks.cs
@@ -5,13 +5,21 @@ namespace NeaUtils.Extensions.ErrorHandling
 {
     internal static class ErrorChecks
     {
+        /// <summary>
+        /// Checks the Type T for Condition exp. If true, an InvalidValueExeption is returned or immidiatly thrown
+        /// </summary>
+        /// <typeparam name="T"> The T item</typeparam>
+        /// <param name="source"> The Extended Item</param>
+        /// <param name="exp">The Lamba Expression that defines the Boolean Operation</param>
+        /// <param name="autothrow">Bool if an Exception should be thrown directly, if Boolean is violated</param>
+        /// <returns> Null or Exception</returns> 
         public static InvalidValueException ConditionalErrorCheck<T>(this T source, Expression<Func<T, bool>> exp, bool autothrow = false)
         {
             if (!exp.Compile()(source))
             {
                 return null;
             }
-            
+
             var ex = new InvalidValueException("The Value " + source + " violated the predicate Function: " + exp.Body);
             if (autothrow)
             {
@@ -21,6 +29,9 @@ namespace NeaUtils.Extensions.ErrorHandling
         }
     }
 
+    /// <summary>
+    /// PlaceHolder Exception Object
+    /// </summary>
     public class InvalidValueException : Exception
     {
         public InvalidValueException()
