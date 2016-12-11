4e239623d28376a5d15c7344ae981efa4e5fbac1
Michael Strickmann <strickmann.michael@outlook.com>
2016-12-11 17:17:43 +0100

    Added Application Utils

---
+++ NeaUtils/Application/ApplicationHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeaUtils.Application
+{
+    public static class ApplicationHelper
+    {
+        public static DirectoryInfo GetApplicationExecutionDirectory()
+        {
+            var location = System.AppDomain.CurrentDomain.BaseDirectory;
+            return location != null ? new DirectoryInfo(location) : null;
+        }
+    }
+}
